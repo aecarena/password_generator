cecbe92dc487b2335850908410dbdfa016605663
aecarena <116913112+aecarena@users.noreply.github.com>
2022-11-28 19:28:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,30 +13,6 @@ var generateButton=document.querySelector("#generate");
 // Add event listener to generate button
 generateButton.addEventListener ("click", writePassword);
 
-// Write password to the #password input
-function writePassword() {
-  var correctPrompts = getprompts();
-  var passwordText = document.querySelector("#password");
-
-  if (correctPrompts){
-    var newpassword = generatePassword();
-    passwordText.value = newpassword;
-  } else {
-    passwordText.value = "";
-  }
- 
-}
-
-function generatePassword() {
-  //generate password based on answers from prompts
-  var password = "";
-  for (var i = 0; i < characterLength; i++) {
-    var randomIndex = math.floor(Math.random() * choiceArray.length)
-    password = password + choiceArray[randomIndex];
-  }
-  return password;
-
-}
 //determine length
 function getprompt() {
   choiceArray = [];
@@ -61,5 +37,31 @@ function getprompt() {
   }
   return true;
 }
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts = getprompts();
+  var passwordText = document.querySelector("#password");
+
+  if (correctPrompts){
+    var newpassword = generatePassword();
+    passwordText.value = newpassword;
+  } else {
+    passwordText.value = "";
+  }
+ 
+}
+
+function generatePassword() {
+  //generate password based on answers from prompts
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+    var randomIndex = math.floor(Math.random() * choiceArray.length)
+    password = password + choiceArray[randomIndex];
+  }
+  return password;
+
+}
+
   
 
